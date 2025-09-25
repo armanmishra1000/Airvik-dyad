cf1b352e5fd12786e6d87dea3fe10ee2a9fcf5df
armanmishra1000 <armanmishra1111@gmail.com>
2025-09-25 13:36:19 +0530

    [dyad] Made the guest management feature functional - wrote 3 file(s)

--- src/app/(app)/guests/page.tsx
+++ src/app/(app)/guests/page.tsx
@@ -1,11 +1,15 @@
-import { mockGuests } from "@/data";
+"use client";
+
+import { useAppContext } from "@/context/app-context";
 import { columns } from "./components/columns";
 import { GuestsDataTable } from "./components/data-table";
 
 export default function GuestsPage() {
+  const { guests } = useAppContext();
+
   return (
     <div className="space-y-4">
-      <GuestsDataTable columns={columns} data={mockGuests} />
+      <GuestsDataTable columns={columns} data={guests} />
     </div>
   );
 }
