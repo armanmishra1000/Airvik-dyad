704d2176e3716f256fa8a280a243137f0b9afd79
armanmishra1000 <armanmishra1111@gmail.com>
2025-09-25 15:06:30 +0530

    [dyad] Fixing a recurring import error - wrote 1 file(s)

--- src/app/(app)/settings/page.tsx
+++ src/app/(app)/settings/page.tsx
@@ -2,7 +2,7 @@
 
 import * as React from "react";
 import { useForm } from "react-hook-form";
-import { zodResolver } from "@hook/resolvers/zod";
+import { zodResolver } from "@hookform/resolvers/zod";
 import * as z from "zod";
 import { toast } from "sonner";
 
